8824aa508d92ecb6193d7e2c655bd2d3dd158fa6
puhongtao <pu.hongtao@yujianjia.com>
2016-12-26 16:27:56 +0800

    config

--- zipkin/src/main/java/com/yuapt/zipkin/ZipkinApplication.java
+++ zipkin/src/main/java/com/yuapt/zipkin/ZipkinApplication.java
@@ -2,8 +2,12 @@ package com.yuapt.zipkin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import zipkin.server.EnableZipkinServer;
 
+@EnableDiscoveryClient
 @SpringBootApplication
+@EnableZipkinServer
 public class ZipkinApplication {
 
 	public static void main(String[] args) {
