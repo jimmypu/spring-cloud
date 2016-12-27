ef9974f3f0c0babe25075105493746a0732bb0b3
puhongtao <pu.hongtao@yujianjia.com>
2016-12-27 16:14:42 +0800

    pr

--- yuapt-service1/src/main/java/com/yuapt/service1/Service1Application.java
+++ yuapt-service1/src/main/java/com/yuapt/service1/Service1Application.java
@@ -2,7 +2,9 @@ package com.yuapt.service1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class Service1Application {
 
